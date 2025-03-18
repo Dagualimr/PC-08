e01a82a33fe50e620bb66a249d3c93eaa9c9d7c2
Dagualimr <dagualimr@correo.url.edu.gt>
2025-03-17 23:00:33 -0600

    Add files via upload

---
+++ Semana #5/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        double a,b;
+        
+        Console.WriteLine("Este programa hara que si el a es mayor que b sea verdadero, de otro modo sera falso.");
+       Console.WriteLine("Ingresa el primer valor a");
+       a=double.Parse(Console.ReadLine());
+
+       Console.WriteLine("Ingrese el segundo valor b");
+       b=double.Parse(Console.ReadLine());
+
+       if (a>b)
+       {
+        Console.WriteLine("Verdadero");
+       }
+       else if (a<b)
+       {
+        Console.WriteLine("Falso");
+       }
+       else
+       {
+        Console.WriteLine("Igualdad");
+       }
+    }
+}
