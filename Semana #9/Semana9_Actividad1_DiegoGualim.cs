6afb6090a6dd3fe94e257185021065c26f1a5f50
Dagualimr <dagualimr@correo.url.edu.gt>
2025-03-17 23:06:56 -0600

    Add files via upload

---
+++ Semana #9/Semana9_Actividad1_DiegoGualim.cs
@@ -0,0 +1,32 @@
+﻿
+class Clase_Semana9
+{
+     static void Main()
+    {
+        bool VF = true;
+        do
+        {
+            Console.WriteLine("Ingrese un número de no mas de 6 digitos");
+            if (int.TryParse(Console.ReadLine(), out int num)|| num < 1 || num > 999999)
+            {
+                for (int i = 2; i * i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine($"{num} no es número primo");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num} es un número primo");
+                    }
+                }
+            }
+            else
+            {
+                    Console.WriteLine("No es válido");
+                VF = false;
+            }
+        }
+        while(VF==true);
+    }
+}
